8ff56600821dc623d2d8f38255a9901e35cead86
Andrei Kucherov <andrei@kucherov.net>
2017-09-26 17:33:03 +1000

    recall, requestOwnership and acceptOwnership have been added

--- contracts/BPC.sol
+++ contracts/BPC.sol
@@ -1,20 +1,26 @@
+// Blockchain product code
+
 pragma solidity ^0.4.11;
 
 contract BPC {
+    enum Status { NEW, RECALLED }
 
     address public brand;
     address public owner;
     address public recipient;
     address public transporter;
+    Status public status;
+    address[] public requests;
 
     function BPC() {
         brand = msg.sender;
         owner = msg.sender;
+        status = Status.NEW;
     }
     
     modifier onlyBrand() {
-	require(msg.sender == brand);
-	_;
+	    require(msg.sender == brand);
+	    _;
     }
 
     modifier onlyOwner() {
@@ -23,15 +29,37 @@ contract BPC {
     }
 
     modifier onlyRecipient() {
-	require(recipient != address(0));
+	    require(recipient != address(0));
         require(msg.sender == recipient);
         _;
     }
     
     modifier onlyTransporter() {
-	require(transporter != address(0));
-	require(msg.sender == transporter);
-	_;
+	    require(transporter != address(0));
+	    require(msg.sender == transporter);
+	    _;
+    }
+
+    function recall() onlyBrand {
+        status = Status.RECALLED;
+    }
+
+    function requestOwnership() {
+        requests.push(msg.sender);
+    }
+
+    function acceptOwnership(address newOwner) onlyOwner {
+        require(newOwner != address(0));
+        require(newOwner != owner);
+        for (uint i = 0; i < requests.length; i++) {
+            if (requests[i] == newOwner) {
+                owner = newOwner;
+            }
+        }
+
+        if (owner == newOwner) {
+            delete requests;
+        }
     }
 
 }
